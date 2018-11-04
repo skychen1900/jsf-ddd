3a986afe84008e007c460d9bd693b0ba050e4f58
vermeer <vermeer@vermeer-THINK>
2018-11-04 18:34:39 +0900

    動的編集を考えてMethodも対象としたが、実際の使用時に実装する事として、まずは除外する（YANGI）

--- src/main/java/ee/domain/annotation/view/ViewContext.java
+++ src/main/java/ee/domain/annotation/view/ViewContext.java
@@ -13,14 +13,14 @@ import java.lang.annotation.Target;
 /**
  * Controllor/Actionにて使用するViewコンテキストを表すために使用します.
  * <p>
- * 本Annotationで指定したフィールド（またはメソッドの戻り値）のClass情報をViewコンテキストとして表明することで、
+ * 本Annotationで指定したフィールドのClass情報をViewコンテキストとして表明することで、
  * ActionとView（Form）の関連付けを明示的に行うために使用します.<br>
  * 例えば、Serviceの検証不正実行時例外の情報と Viewのフィールドを関連付けるために使用します.
  *
  * @author Yamashita,Takahiro
  */
 @Documented
-@Target(value = {ElementType.FIELD, ElementType.METHOD})
+@Target(value = {ElementType.FIELD})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface ViewContext {
 

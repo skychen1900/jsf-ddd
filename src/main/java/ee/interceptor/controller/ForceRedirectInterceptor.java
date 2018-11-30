5dadb1b76bab560a559005aebce67e91489f472f
vermeer <vermeer@vermeer-THINK>
2018-11-30 17:14:10 +0900

    Copyrightを修正

--- src/main/java/ee/interceptor/controller/ForceRedirectInterceptor.java
+++ src/main/java/ee/interceptor/controller/ForceRedirectInterceptor.java
@@ -1,16 +1,28 @@
 /*
- * Copyright(C) 2013 Sanyu Academy All rights reserved.
  *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ *  Copyright © 2018 Yamashita,Takahiro
  */
 package ee.interceptor.controller;
 
-import spec.annotation.presentation.controller.Action;
 import javax.annotation.Priority;
 import javax.enterprise.context.Dependent;
 import javax.inject.Inject;
 import javax.interceptor.AroundInvoke;
 import javax.interceptor.Interceptor;
 import javax.interceptor.InvocationContext;
+import spec.annotation.presentation.controller.Action;
 import spec.interfaces.infrastructure.CurrentViewContext;
 
 @Action
--- src/main/java/exsample/jsf/presentation/userregistration/UserUpdateAction.java
+++ src/main/java/exsample/jsf/presentation/userregistration/UserUpdateAction.java
@@ -1,13 +1,29 @@
+/*
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ *  Copyright © 2018 Yamashita,Takahiro
+ */
 package exsample.jsf.presentation.userregistration;
 
-import spec.annotation.presentation.controller.Controller;
-import spec.annotation.presentation.controller.EndConversation;
-import spec.annotation.presentation.controller.ViewContext;
 import exsample.jsf.application.service.UpdateUser;
 import exsample.jsf.application.service.UserService;
 import exsample.jsf.domain.model.user.User;
 import exsample.jsf.domain.model.user.UserId;
 import javax.inject.Inject;
+import spec.annotation.presentation.controller.Controller;
+import spec.annotation.presentation.controller.EndConversation;
+import spec.annotation.presentation.controller.ViewContext;
 
 @Controller
 public class UserUpdateAction {
